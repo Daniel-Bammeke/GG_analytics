1240bc3b8e7b55589203177ae31af5c492a598bf
bammeked <bammeked@coventry.ac.uk>
2022-12-07 17:41:24 +0000

    sources defined

---
+++ gg_dbt_project/models/report/stg_partners.sql
@@ -0,0 +1,16 @@
+with partners as
+(
+    select *
+    from {{ source('raw_data', 'partners') }}
+)
+
+select 
+    id, 
+	to_timestamp((created_at :: numeric)/1E+9) created_at,
+	to_timestamp((updated_at :: numeric)/1E+9) updated_at,
+	partner_type,
+    case
+       when lead_sales_contact = '0' then 'Not Assigned'
+       else lead_sales_contact
+    end lead_sales_contact
+from partners
